4c94677d0f14bedec330dfa4925ed327b4593da5
sIIIck <siddharthtdd123@gmail.com>
2024-02-02 18:47:00 +0545

    Implement imperial to si

--- src/routes/imperial_to_si_converter/+page.svelte
+++ src/routes/imperial_to_si_converter/+page.svelte
@@ -1,10 +1,45 @@
 <script>
 import Navbar from "$lib/Navbar.svelte";
+import {number_checker, 
+        pounds_to_kilos, 
+        kilos_to_pounds
+       } from "./weight_funcs";
 
-$: target_id = "length";
-function option_picker(e){
-    target_id = e.target.value;
+//input declaration
+$: input_pounds = undefined;
+$: input_kilos = undefined;
+
+
+//webloop declaration
+$: weight_web_loop = (e) => {
+    let local_target_id = e.target.id;
+    if (local_target_id == "pounds"){
+    input_pounds = number_checker(input_pounds);
+    input_kilos = pounds_to_kilos(input_pounds);
+
+    }
+    if (local_target_id == "kilos"){
+    input_kilos = number_checker(input_kilos);
+    input_pounds = kilos_to_pounds(input_kilos);
+    }
 }
+
+
+$: target_id = "weight";
+$: option_picker = (e) => {
+    target_id = e.target.value;
+    console.log(e);
+}
+
+$: main_web_loop = (e) => {
+    if (target_id == "weight"){
+        weight_web_loop(e);
+    }
+
+}
+
+
+
 
 </script>
 
@@ -12,12 +47,20 @@ function option_picker(e){
 <select name="select" id="units" on:change={option_picker}>
     <option >Select</option>
     <option disabled>----------</option>
+    <option value="weight">Weight</option>
     <option value="length">Length</option>
     <option value="area">Area</option>
-    <option value="volume">Volume</option>
 </select>
 <main>
 <div class="hero">
+
+{#if target_id == "weight"}
+<span>Pounds</span>
+<input type="text" bind:value={input_pounds} on:input={main_web_loop} id="pounds" placeholder="0"/>
+<span>Kilograms</span>
+<input type="text" id="kilos" placeholder="0" bind:value={input_kilos} on:input={main_web_loop}/>
+{/if}
+
 {#if target_id == "length"}
 <span>Meter</span>
 <input type="text"/>
